62c78fdd3bd66699fee0b2d878ae4b285d51766c
Achim Strohm <achim.strohm@websedit.de>
2022-08-08 15:26:49 +0200

    Release version 3.0.3

--- Classes/Hook/AfterSaveHook.php
+++ Classes/Hook/AfterSaveHook.php
@@ -7,30 +7,30 @@ namespace Websedit\WeCookieConsent\Hook;
  */
 class AfterSaveHook
 {
-    public function processDatamap_preProcessFieldArray(&$fieldArray, $table, $id, &$dataHandler)
+    public function processDatamap_preProcessFieldArray(&$fieldArray, $table, $id, \TYPO3\CMS\Core\DataHandling\DataHandler &$dataHandler)
     {
         if ($table === 'tx_wecookieconsent_domain_model_service' && $fieldArray['provider'] === 'google-tagmanager-service') {
-            if ($fieldArray['title']) {
+            if (isset($fieldArray['title'])) {
                 $title = $fieldArray['title'];
             } else {
                 $title = $id;
             }
 
-            if ($fieldArray['gtm_tag_title'] === '') {
+            if (isset($fieldArray['gtm_tag_title'])) {
                 $fieldArray['gtm_tag_title'] = $title . ' - Tag';
             }
 
-            if ($fieldArray['gtm_trigger_title'] === '') {
+            if (isset($fieldArray['gtm_trigger_title'])) {
                 $fieldArray['gtm_trigger_title'] = $title . ' - Trigger';
             }
-            if ($fieldArray['gtm_trigger_name'] === '') {
+            if (isset($fieldArray['gtm_trigger_name'])) {
                 $fieldArray['gtm_trigger_name'] = $fieldArray['provider'] . '-' . $id;
             }
 
-            if ($fieldArray['gtm_variable_title'] === '') {
+            if (isset($fieldArray['gtm_variable_title'])) {
                 $fieldArray['gtm_variable_title'] = $title . ' - Variable';
             }
-            if ($fieldArray['gtm_variable_name'] === '') {
+            if (isset($fieldArray['gtm_variable_name'])) {
                 $fieldArray['gtm_variable_name'] = $fieldArray['provider'] . '-' . $id;
             }
         }
--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'clearcacheonload' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '3.0.2',
+  'version' => '3.0.3',
   'constraints' => 
   array (
     'depends' => 
