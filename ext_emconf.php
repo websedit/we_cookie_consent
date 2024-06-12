b26970c70f26c26b9e56222d3bd854400d5ce331
webseditag <github@websedit.de>
2024-06-12 09:53:12 +0200

    Fixed initialisation of dataLayer without google service in ConsentController.js

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'clearcacheonload' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '2.3.5',
+  'version' => '2.3.6',
   'constraints' => 
   array (
     'depends' => 
