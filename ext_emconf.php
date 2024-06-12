e6812204376aed571d2ed88f0fc131b811182c18
Achim Strohm <achim.strohm@websedit.de>
2024-06-12 11:28:28 +0200

    Change changelog and description to add new cookie icon. Configuration GF.

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'clearcacheonload' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '4.0.5',
+  'version' => '4.0.6',
   'constraints' => 
   array (
     'depends' => 
