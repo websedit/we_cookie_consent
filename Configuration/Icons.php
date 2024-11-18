b0d69a9aa650064f35381edbf5ed9fa2fd198a84
Stefan Sofka <stefan.sofka@websedit.de>
2024-11-18 12:23:44 +0100

    Modify version to v5.0.0 for typo3 13.4 and remove compatibility for v10 and v11

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'we-jobs-extension-icon' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:we_jobs/Resources/Public/Icons/ext_icon.svg',
+    ],
+];
