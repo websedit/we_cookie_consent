f18ca0f905ba87b059fa0d45e44dfba2b28ee849
Stefan Sofka <stefan.sofka@websedit.de>
2024-11-19 15:46:50 +0100

    move if-condition for cookieIconPermanentlyAvailable into function createTooltip()

--- Resources/Public/JavaScript/HoverCookie/ConsentCookieSettings.js
+++ Resources/Public/JavaScript/HoverCookie/ConsentCookieSettings.js
@@ -1,28 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    if ((cookieIconPermanentlyAvailable === '1') && (checkCookieExists('klaro'))) {
+    if (checkCookieExists('klaro')) {
         createTooltip();
     }
 
-    if (cookieIconPermanentlyAvailable === '1') {
-        setTimeout(function () {
-            const klaroDiv = document.getElementById('klaro');
-            const buttons = klaroDiv.querySelectorAll('button');
+    setTimeout(function () {
+        const klaroDiv = document.getElementById('klaro');
+        const buttons = klaroDiv.querySelectorAll('button');
 
-            function handleClick(event) {
-                createTooltip();
-                if (document.querySelector('#klaro .cookie-notice') == null && document.querySelector('#klaro .cookie-modal') == null) {
-                }
+        function handleClick(event) {
+            createTooltip();
+            if (document.querySelector('#klaro .cookie-notice') == null && document.querySelector('#klaro .cookie-modal') == null) {
             }
+        }
 
-            buttons.forEach(button => {
-                button.addEventListener('click', handleClick);
-            });
-
-
+        buttons.forEach(button => {
+            button.addEventListener('click', handleClick);
         });
-
-    }
+    });
 
     setTimeout(function () {
         if (document.querySelector('.cm-link')) {
@@ -67,33 +62,35 @@ function checkCookieExists(cookieName) {
 }
 
 function createTooltip() {
-    let tooltip = document.createElement('div');
-    let imgTag = document.createElement('img');
-    let iconContainer = document.createElement('div');
+    if (cookieIconPermanentlyAvailable === '1') {
+        let tooltip = document.createElement('div');
+        let imgTag = document.createElement('img');
+        let iconContainer = document.createElement('div');
 
-    imgTag.src = cookieSettingsImgPathDefault;
-    imgTag.className = 'consent-img js-showConsentModal';
-    imgTag.style.cssText = 'margin-right:0px;width:85px;height:85px;'; //Distance to the text
-    imgTag.onmouseover = function () {
-        this.src = cookieSettingsImgPathHover;
-        tooltip.style.display = 'block'; //Show tooltip when hovering over
-    };
-    imgTag.onmouseout = function () {
-        this.src = cookieSettingsImgPathDefault;
-        tooltip.style.display = 'none'; //Hide tooltip when hovering over
-    };
+        imgTag.src = cookieSettingsImgPathDefault;
+        imgTag.className = 'consent-img js-showConsentModal';
+        imgTag.style.cssText = 'margin-right:0px;width:85px;height:85px;'; //Distance to the text
+        imgTag.onmouseover = function () {
+            this.src = cookieSettingsImgPathHover;
+            tooltip.style.display = 'block'; //Show tooltip when hovering over
+        };
+        imgTag.onmouseout = function () {
+            this.src = cookieSettingsImgPathDefault;
+            tooltip.style.display = 'none'; //Hide tooltip when hovering over
+        };
 
-    tooltip.textContent = translatedButtonTextCookieSettings;
-    tooltip.classList.add('consent-cookie-text');
-    tooltip.style.cssText = 'display:none;';
+        tooltip.textContent = translatedButtonTextCookieSettings;
+        tooltip.classList.add('consent-cookie-text');
+        tooltip.style.cssText = 'display:none;';
 
-    iconContainer.classList.add('consent-cookie')
-    iconContainer.style.cssText = 'display:block;';
-    iconContainer.style.display = 'flex';
+        iconContainer.classList.add('consent-cookie')
+        iconContainer.style.cssText = 'display:block;';
+        iconContainer.style.display = 'flex';
 
-    // Add the image and the tooltip to the container
-    iconContainer.appendChild(imgTag);
-    iconContainer.appendChild(tooltip);
+        // Add the image and the tooltip to the container
+        iconContainer.appendChild(imgTag);
+        iconContainer.appendChild(tooltip);
 
-    document.body.appendChild(iconContainer);
+        document.body.appendChild(iconContainer);
+    }
 }
