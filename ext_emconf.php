e76dde3fdc2344e39c079e4d50ffca5e234b5bf0
Achim Strohm <achim.strohm@websedit.de>
2025-03-03 10:54:43 +0100

    Changed version in ext_emconf and changelog

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'clearcacheonload' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '5.0.0',
+  'version' => '5.0.2',
   'constraints' => 
   array (
     'depends' => 
