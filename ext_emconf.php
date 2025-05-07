f693439384faf9c8b8e59bfafb96d644eee9da04
webseditag <github@websedit.de>
2025-05-07 11:41:05 +0200

    Bugfix, if all consent types in a service defined to denied + Change structure of ConsentController.js

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'clearcacheonload' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '4.1.2',
+  'version' => '4.1.3',
   'constraints' => 
   array (
     'depends' => 
