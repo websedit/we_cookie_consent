e06f78a8782600bfcf637c4573bcc1eca7b1338f
Achim Strohm <achim.strohm@websedit.de>
2025-05-07 13:38:24 +0200

    Release v5.0.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
     'uploadfolder' => false,
     'clearcacheonload' => false,
     'clearCacheOnLoad' => 0,
-    'version' => '5.0.4',
+    'version' => '5.0.5',
     'constraints' =>
         array (
             'depends' =>
